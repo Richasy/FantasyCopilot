727c9279e28e6bd3cb2d437a27b1a5f49ce7cb97
Richasy <Thansy@foxmail.com>
2023-06-25 10:03:06 +0800

    Fix baidu translate issue

--- src/Services/Services.Desktop/BaiduTranslateService/BaiduTranslateService.cs
+++ src/Services/Services.Desktop/BaiduTranslateService/BaiduTranslateService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -54,6 +55,46 @@ public sealed partial class BaiduTranslateService : ITranslateService
             sourceLanguage = "auto";
         }
 
+        text = text.Replace("\r", "\n").Replace("\n\n", "\n");
+        var textBuilder = new StringBuilder();
+        var resultBuilder = new StringBuilder();
+        foreach (var item in text.Split('\n'))
+        {
+            if (textBuilder.Length + item.Length > MaxTextLength)
+            {
+                // Due to the API request rate limit,
+                // a translation is requested at most once per second,
+                // so additional blocking is required here
+                if (resultBuilder.Length > 0)
+                {
+                    await Task.Delay(1000, cancellationToken);
+                }
+
+                var tempResult = await TranslateInternalAsync(textBuilder.ToString(), sourceLanguage, targetLanguage, cancellationToken);
+                resultBuilder.AppendLine(tempResult);
+                textBuilder.Clear();
+            }
+
+            textBuilder.AppendLine(item);
+        }
+
+        if (textBuilder.Length > 0)
+        {
+            if (resultBuilder.Length > 0)
+            {
+                await Task.Delay(1000, cancellationToken);
+            }
+
+            var tempResult = await TranslateInternalAsync(textBuilder.ToString(), sourceLanguage, targetLanguage, cancellationToken);
+            resultBuilder.AppendLine(tempResult);
+        }
+
+        return resultBuilder.ToString();
+    }
+
+    private async Task<string> TranslateInternalAsync(string text, string sourceLanguage, string targetLanguage, CancellationToken cancellationToken)
+    {
+        text = text.Replace("\r", "\n").Replace("\n\n", "\n");
         var dict = new Dictionary<string, string>
         {
             { "q", text },
@@ -77,14 +118,22 @@ public sealed partial class BaiduTranslateService : ITranslateService
         }
         else
         {
+            var sb = new StringBuilder();
             var hasResult = jele.TryGetProperty("trans_result", out var transResultEle);
             if (!hasResult)
             {
                 throw new Exception("No result.");
             }
 
-            transResultEle.EnumerateArray().FirstOrDefault().TryGetProperty("dst", out var dstEle);
-            return dstEle.ToString();
+            foreach (var item in transResultEle.EnumerateArray())
+            {
+                if (item.TryGetProperty("dst", out var dstEle))
+                {
+                    sb.AppendLine(dstEle.ToString());
+                }
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
